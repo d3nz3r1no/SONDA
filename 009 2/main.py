71e4477fd80e516523145feb391b48612acc42c4
AquA <kibertronprimus@gmail.com>
2025-04-11 19:11:42 +0300

    fix

--- 009 2/main.py
+++ 009 2/main.py
@@ -4,8 +4,9 @@ from datetime import datetime
 from telebot import TeleBot
 from db import Database
 from properties import TOKEN, ADMIN_ID
-import buttoms_sys
 import cmd
+import buttoms_sys
+import passwords_sys
 
 # Настройка логирования
 logging.basicConfig(
