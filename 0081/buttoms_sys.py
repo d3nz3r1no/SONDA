7a5fb587ba9404736b7cfec422b784afe4b9c374
AquA <kibertronprimus@gmail.com>
2025-04-11 03:09:47 +0300

    mini fix

--- 0081/buttoms_sys.py
+++ 0081/buttoms_sys.py
@@ -103,7 +103,7 @@ def handle_about(message):
         "Я - SONDA, создана для удобства человека.\n"
         "Группа тестировщиков - https://t.me/sondatest\n"
         "Канал про меня - https://t.me/sondachanel\n\n"
-        "Версия: 0.0.8"
+        "Версия: 0.0.81"
     )
     bot.reply_to(message, about_text, reply_markup=markup)
     log_action(message.chat.id, "Просмотр информации о боте")
