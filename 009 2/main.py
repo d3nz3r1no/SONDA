aa65af0a085d1ba653f49864da3135cb00a8fb80
AquA <kibertronprimus@gmail.com>
2025-04-11 19:02:20 +0300

    Fix

--- 009 2/main.py
+++ 009 2/main.py
@@ -6,9 +6,6 @@ from db import Database
 from properties import TOKEN, ADMIN_ID
 import buttoms_sys
 import cmd
-import sys
-
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 # Настройка логирования
 logging.basicConfig(
