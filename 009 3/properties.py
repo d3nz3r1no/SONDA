dab7e5c20fb2b6baeacebe1c753223a2d6128278
AquA <kibertronprimus@gmail.com>
2025-04-12 06:30:29 +0300

    Новая разработка

---
+++ 009 3/properties.py
@@ -0,0 +1,9 @@
+import telebot
+
+# Конфигурационные константы
+TOKEN = "7463516138:AAFpFJO-q-iFX-xU7j6OBpUbrNPiK_DCVVU"
+ADMIN_ID = 1331203510
+DB_BACKUP_FILE = "sonda_backup.db"
+BACKUP_INTERVAL_DAYS = 1
+bot = telebot.TeleBot(TOKEN)
+user_calculations = {}  # Хранение текущих вычислений пользователей
